538601c19eff81a8d5f53d47d35cf25e41fe9910
Magaivin <seu-email@exemplo.com>
2025-05-09 19:45:19 -0300

    jhjjhjhjhjh

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,22 @@ import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } fr
 import { provideRouter, withPreloading, withInMemoryScrolling, withRouterConfig } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+// Firebase
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage'; // Adicionado Storage como exemplo, remova se não usar
+
+// ng-recaptcha (para v2/invisible)
+import { RecaptchaFormsModule, RecaptchaModule, RecaptchaSettings, RECAPTCHA_SETTINGS } from 'ng-recaptcha';
+
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { ProjectModule } from './features/project/project.module';
 import { routes } from './app.routes';
 import { OptimizedPreloadingStrategy } from './core/services/preload-strategy.service';
+import { environment } from '../environments/environment'; // Importar environment
 
 /**
  * Configuração principal da aplicação Angular
@@ -15,6 +26,7 @@ import { OptimizedPreloadingStrategy } from './core/services/preload-strategy.se
  * - Roteador com preloading estratégico e configurações de rolagem
  * - Hydration do cliente para SSR
  * - Interceptor HTTP para cache
+ * - Provedores para Firebase e Recaptcha
  */
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -44,6 +56,19 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay()),
     
     // Cliente HTTP com suporte a interceptores
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+
+    // Configuração do Firebase
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()), // Remova ou ajuste conforme o uso
+
+    // Configuração do ng-recaptcha
+    importProvidersFrom(RecaptchaFormsModule, RecaptchaModule),
+    {
+      provide: RECAPTCHA_SETTINGS,
+      useValue: { siteKey: environment.recaptcha.siteKey } as RecaptchaSettings,
+    },
   ]
 };
